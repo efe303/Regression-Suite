56f3b6a42ec5a3e33a2b14f98a957743f209e3ea
efe303 <scientistefe@gmail.com>
2018-10-27 20:52:26 -0400

    json

--- Pages/signUpForEmail.page.js
+++ Pages/signUpForEmail.page.js
@@ -6,46 +6,6 @@ require ('../Utilities/CustomLocators.js');
 //var base = require ('../Utilities/Base.js')
 
 var SignUpForEmail=function(){
-<<<<<<< HEAD
-  this.mailImage=element(by.ngShow("newsletterFormCtrl.uiLocation === 'email-news'"));
-  this.heading1=$('.layout-wrapper.ng-scope>section>div>h1');
-  this.para=$('.layout-wrapper.ng-scope>section>div>p');
-  this.inputBoxes=$$('.form-input>input');
-  this.signUpButton=element.all(by.buttonText('Sign Up'));  
-  this.thanksForSignUp=$$('.message>h4');
-  this.unsubscribe=element.all(by.linkText('Unsubscribe Here'));
-<<<<<<< HEAD
-//  this.unsubscribeInputBox=element(by.css("body > table > tbody > tr > td > form > p:nth-child(6) > input[type='text']"));
-//  this.radioYes=$("tbody > tr:nth-child(1) > td:nth-child(1) > input[type='radio']");
-//  this.submitUnsubscribe=$("input[type='image']");
-//  this.unsubscribeProcessed=$('tbody p');
-//  this.privacyPolicy2=$$('.privacy-policy');
-//  this.needToLeave=$$('.unsubscribe p');
-//  this.displayRequired=$$('.error-message.ng-binding');
-//  this.radioNo=$(" tbody > tr:nth-child(2) > td:nth-child(1) > input[type='radio']")
-//  this.Checkbox=$$('.checkbox> label');
-//  this.signUpNowButton=$$('.btn.blue');
-//  this.top=$('#top');
-//  this.contactInfo=element(by.linkText('Contact Information'));
-//  this.termsOfUse=element.all(by.linkText('/terms-of-use'));
-//  this.socialMedia=$$('.header-banner-container a');
-=======
-  this.unsubscribeInputBox=element(by.css("body > table > tbody > tr > td > form > p:nth-child(6) > input[type='text']"));
-  this.radioYes=$("tbody > tr:nth-child(1) > td:nth-child(1) > input[type='radio']");
-  this.submitUnsubscribe=$("input[type='image']");
-  this.unsubscribeProcessed=$('tbody p');
-  this.privacyPolicy2=$$('.privacy-policy');
-  this.needToLeave=$$('.unsubscribe p');
-  this.displayRequired=$$('.error-message.ng-binding');
-  this.radioNo=$(" tbody > tr:nth-child(2) > td:nth-child(1) > input[type='radio']")
-  this.Checkbox=$$('.checkbox> label');
-  // this.signUpNowButton=$$('.btn.blue');
-  // // this.top=$('#top');
-  // // this.contactInfo=element(by.linkText('Contact Information'));
-  // // this.termsOfUse=element.all(by.linkTesxt('/terms-of-use'));
-  // // this.socialMedia=$$('.header-banner-container a');
->>>>>>> 6c6afc3bfcf23d67abceff8f3f1919ceebc6f736
-=======
   // this.mailImage=element(by.ngShow("newsletterFormCtrl.uiLocation === 'email-news'"));
   // this.heading1=$('.layout-wrapper.ng-scope>section>div>h1');
   // this.para=$('.layout-wrapper.ng-scope>section>div>p');
@@ -67,7 +27,6 @@ var SignUpForEmail=function(){
   // // // this.contactInfo=element(by.linkText('Contact Information'));
   // // // this.termsOfUse=element.all(by.linkTesxt('/terms-of-use'));
   // // // this.socialMedia=$$('.header-banner-container a');
->>>>>>> d92a7e1846a421dafd2bb38d2676e5f58f4d699b
 };
 module.exports = new SignUpForEmail();
 
